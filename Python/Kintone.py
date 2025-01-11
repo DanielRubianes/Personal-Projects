f4ee60a0984c29274eafdaef9484029899addb4b
DanielRubianes <DanielRubianes1352@gmail.com>
2025-01-11 13:23:05 -0500

    Working code

--- Python/Kintone.py
+++ Python/Kintone.py
@@ -36,9 +36,11 @@ class Kintone_App:
             }
         )
         response_dict = json.loads( response.data.decode('utf-8') )
-        print(query + f' order by $id asc limit {chunk_size}')
+        print('GET query: ' + query + f' order by $id asc limit {chunk_size}')
         if 'records' not in response_dict:
             pprint(response.data)
+            print('No records found!')
+            return None
         records: list[dict] = response_dict['records']
         total_count = int(response_dict['totalCount'])
         if total_count > chunk_size:
@@ -47,15 +49,20 @@ class Kintone_App:
         else:
             return records
         
-    """Runs multiple get requests to get more than 500 records"""
+    """Updates one field to a specified value for all records that match the given query"""
     def update_records(self, field: str, new_value: Any, query: str='', _last_record_id: int = None) -> bool:
 
         records_to_update: list[ dict[str, dict[str, Any] ] ] = self.get_records([field], query)
+
+        if not records_to_update:
+            print('No records match query!')
+            return False
 
         update_ids: list[str] = \
         [
             record['$id']['value']
             for record in records_to_update
+            if record[field]['value'] != new_value
         ]
 
         record_updates: list[ dict[str, dict[str, Any] ] ] = \
@@ -71,8 +78,9 @@ class Kintone_App:
         query = f'({query}) and ' + bulk_query if query and bulk_query else query + bulk_query
 
         chunk_size = 100
-        for i in range(0, len(record_updates), 100):
-            chunk = record_updates[i:i+100]
+        update_count = 0
+        for i in range(0, len(record_updates), chunk_size):
+            chunk = record_updates[i:i+chunk_size]
 
             payload = \
             {
@@ -90,24 +98,37 @@ class Kintone_App:
             response_dict = json.loads( response.data.decode('utf-8') )
         
             if 'records' in response_dict:
-                ids = [record['id'] for record in response_dict['records']]
+                update_count += len(response_dict['records'])
             else:
                 print(response.data)
                 print('No records updated!')
                 return False
+        print(f'{update_count} records updated')
         return True
 
 
 
 test_app = Kintone_App('https://throughthetrees.kintone.com/k/v1/', api_token='G6cy4Bt98XYpveGhZkHjhK06EzHPUfK0INVIcbSi', app_id=13)
 
-records: list[ dict[str, dict[str, str] ] ] = test_app.get_records(['Record_Status', 'Record_number', 'Text_1'], '$id = 1')
+
+devices_app = Kintone_App('https://throughthetrees.kintone.com/k/v1/', api_token='', app_id=4)
+# Fields
+# Drop_down_1: Battery Status
+# Text_7: Model
+# Bay : Bay
+# Drop_down_2: Item Status
+# Text: Asset Tag
+
+shelf_query = 'Text_7 in ("ProBook 650 G2", "ENVY x360 m6", "Elitebook 840 G3", "Elitebook 840 G5", "Elitebook 850 G3", "Elitebook 850 G5", "Elitebook 8470p", "ROG G752V", "Pavillion G6")'
+shelf_query += ' and Drop_down_2 in ("Waiting on Parts", "Waiting on Parts (Ordered)")'
+shelf_query += ' and Bay = ""'
+
+records: list[ dict[str, dict[str, str] ] ] = devices_app.get_records([''], shelf_query)
 
 
 # Get records and print info out
-# print(response.data)
-selected_records: list[int] = []
 all_fields = []
+asset_tags: list[str] = []
 for record in records:
     # print('=======================')
     # print(record)
@@ -115,15 +136,15 @@ for record in records:
         if field not in all_fields:
             all_fields.append(field)
         value = value['value']
-        if field == 'Record_number':
-            selected_records.append(value)
-# print( sorted(selected_records, key=lambda x: int(x)) )
+        if field is 'Text':
+            asset_tags += value
+
+# print( sorted(records, key=lambda x: int(x)) )
 print(all_fields)
-# print(len(selected_records))
-# print(response['totalCount'])
+print(len(records))
 
 # Update records
-test_app.update_records('Test_2', 'Test Value', '')
+devices_app.update_records('Bay', 'Needs Parts Shelf', shelf_query)
 
 # Create 500 ton of records
 # duped_rows = [
@@ -142,7 +163,7 @@ test_app.update_records('Test_2', 'Test Value', '')
 #     }
 #     response: BaseHTTPResponse = http.request(
 #         "POST", "https://throughthetrees.kintone.com/k/v1/records.json",
-#         headers={"X-Cybozu-API-Token": "G6cy4Bt98XYpveGhZkHjhK06EzHPUfK0INVIcbSi",
+#         headers={"X-Cybozu-API-Token": "",
 #             "Content-Type": "application/json"},
 #         body=json.dumps(payload).encode('utf-8')
 #     )
