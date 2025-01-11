503db49dc72081fa7f1fbc67d02da36023f3c0a8
DanielRubianes <DanielRubianes1352@gmail.com>
2025-01-11 11:11:05 -0500

    Replace old function

--- Python/Kintone.py
+++ Python/Kintone.py
@@ -15,33 +15,13 @@ class Kintone_App:
     api_token: str
     app_id: int
 
-    """Calls get records request (limited to 500 records)"""
-    def get_records(self, fields: list[str], query: str=None):
-        response: BaseHTTPResponse = http.request(
-            "GET", self.url + "records.json",
-            headers={"X-Cybozu-API-Token": self.api_token},
-            fields=\
-            {
-                "app": self.app_id,
-                "fields": ','.join(fields),
-                "query": query,
-                "totalCount": True
-            }
-        )
-        response_dict = json.loads( response.data.decode('utf-8') )
-        total_count = response_dict['totalCount']
-        records: list[dict] = response_dict['records']
-        return records
-
-
     """Runs multiple get requests to get more than 500 records"""
-    def get_bulk_records(self, fields: list[str], query: str='', _last_record_id: int = None):
-        
-        bulk_query = '$id > ' + _last_record_id if _last_record_id >= 0 else ''
+    def get_records(self, fields: list[str], query: str='', _last_record_id: int = None):
+
+        bulk_query = f'$id > {_last_record_id}' if _last_record_id else ''
         query = f'({query}) and ' + bulk_query if query and bulk_query else query + bulk_query
 
         chunk_size = 500
-        query += f' order by $id asc limit {chunk_size}'
 
         response: BaseHTTPResponse = http.request(
             "GET", self.url + "records.json",
@@ -49,23 +29,28 @@ class Kintone_App:
             fields=\
             {
                 "app": self.app_id,
-                "fields": ','.join(fields+'$id'),
-                "query": query,
+                "fields": ','.join(fields+['$id']),
+                "query": query + f' order by $id asc limit {chunk_size}',
                 "totalCount": True
             }
         )
         response_dict = json.loads( response.data.decode('utf-8') )
+        print(query + f' order by $id asc limit {chunk_size}')
+        if 'records' not in response_dict:
+            pprint(response.data)
         records: list[dict] = response_dict['records']
-        total_count = response_dict['totalCount']
+        total_count = int(response_dict['totalCount'])
         if total_count > chunk_size:
-            last_record_id = max(record['$id'] for record in records)
-            return records + self.get_bulk_records(self, fields, query, last_record_id)
+            last_record_id = max(int(record['$id']['value']) for record in records)
+            return records + self.get_bulk_records(fields, query, last_record_id)
+        else:
+            return records
 
 
-            
-test_app = Kintone_App('https://throughthetrees.kintone.com/k/v1/', api_token='XXXXXXXXXXXXXXXX', app_id=13)
 
-records = test_app.get_records(['Record_Status', 'Record_number', 'Text_1'], 'Record_Status in ("Need to Order")')
+test_app = Kintone_App('https://throughthetrees.kintone.com/k/v1/', api_token='', app_id=13)
+
+records = test_app.get_bulk_records(['Record_Status', 'Record_number', 'Text_1'], 'Record_Status in ("Need to Order")')
 
 # Sending a GET request and getting back response as HTTPResponse object.
 
@@ -81,8 +66,10 @@ for record in records:
         if field == 'Record_number':
             selected_records.append(value)
 
-print(selected_records)
+print( sorted(selected_records, key=lambda x: int(x)) )
 print(len(selected_records))
+            
+
 # print(response['totalCount'])
 
 # Create a shit ton of records
